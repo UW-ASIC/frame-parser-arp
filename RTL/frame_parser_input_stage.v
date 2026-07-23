7b7bf1ba5d2279dd8f137a3dc563580607edad56
Joanna Sebastiampillai <j4sebast@uwaterloo.ca>
2026-07-23 00:41:35 -0400

    cleaed up input stage

--- RTL/frame_parser_input_stage.v
+++ RTL/frame_parser_input_stage.v
@@ -12,7 +12,7 @@ module frame_parser_input (
     input wire        tuser,
 
     // outputs to MAC
-    output wire       tready,
+    output wire       out_tready,
     
     // inputs from frame control + header datapath
     input wire        frame_ready,
@@ -26,29 +26,47 @@ module frame_parser_input (
     output reg        out_tuser
 );
 
-  reg processing;
-
-  always @(posedge clk or negedge rst_n) begin
-    if (!rst_n || tlast) processing <= 1'b0;
-    else if (tvalid) processing <= 1'b1;
-  end
+  reg [63:0] reg_tdata;
+  reg [7:0]  out_tkeep;
+  reg        out_tvalid;
+  reg        out_tlast;
+  reg        out_tuser;
 
   always @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
+      reg_tdata <= 64'b0;
+      reg_tkeep <= 8'b0;
+      reg_tvalid <= 1'b0;
+      reg_tlast <= 1'b0;
+      reg_tuser <= 1'b0;
+
       out_tdata <= 64'b0;
       out_tkeep <= 8'b0;
       out_tvalid <= 1'b0;
       out_tlast <= 1'b0;
       out_tuser <= 1'b0;
-    end else if (tvalid || processing) begin
-      out_tdata <= tdata;
-      out_tkeep <= tkeep;
-      out_tvalid <= tvalid || processing;
-      out_tlast <= tlast;
-      out_tuser <= tuser;
-    end
-  end
 
-  assign tready = processing || (frame_ready && header_ready);
+      out_tready <= 1'b0;
+    end else if (tvalid) begin
+      reg_tdata <= tdata;
+      reg_tkeep <= tkeep;
+      reg_tvalid <= tvalid;
+      reg_tlast <= tlast;
+      reg_tuser <= tuser;
+
+      out_tdata <= reg_tdata;
+      out_tkeep <= reg_tkeep;
+      out_tvalid <= reg_tvalid;
+      out_tlast <= reg_tlast;
+      out_tuser <= reg_tuser;
+
+      out_tready <= frame_ready && header_ready;
+    end else begin
+      reg_tdata <= 64'b0;
+      reg_tkeep <= 8'b0;
+      reg_tvalid <= 1'b0;
+      reg_tlast <= 1'b0;
+      reg_tuser <= 1'b0;
+  end
 
 endmodule
